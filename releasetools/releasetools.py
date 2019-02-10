c14412942c12c338f71106ff88e1e58e4666f9d1
LuK1337 <priv.luk@gmail.com>
2019-02-10 12:08:15 +0100

    kirin970-common: Assert on installation on devices with newer firmware
    * Currently our kernel does not support devices without
      ro.boot.slot_suffix (aka CONFIG_HISI_AB_PARTITION) so
      let's just abort the installation if the prop isn't
      set to _a.
    
    Change-Id: Ie030d4b4acf87dbcfa71b47eb7806f10544772e4

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -32,3 +32,5 @@ def AddVendorAssertion(info, input_zip):
     if m:
         cmd = 'assert(huawei.verify_vendor_build_id("' + m.group(1).rstrip() + '") == "1");'
         info.script.AppendExtra(cmd)
+
+    info.script.AssertOemProperty("ro.boot.slot_suffix", ["_a"], True)
